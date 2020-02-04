03b22c711b06c6fd1ecb6739703981d3549b3a40
domero2 <stefanbelmond@gmail.com>
2020-02-04 10:20:19 +0100

    Update Pyscarping_am.py

--- Pyscarping_am.py
+++ Pyscarping_am.py
@@ -1,16 +1,47 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
+#Funcion responsible for create list of a elements in web
+def creat_file_from_web(url):
+    response = requests.get(url)
+    print(response)
+    soup = BeautifulSoup(response.text,features="lxml")
+    all_a_atribute_on_side=soup.findAll('a')
+    return all_a_atribute_on_side
 
-#We are checking all a 
-url = 'http://amadeuszmajcher.pl/blog/'
-response = requests.get(url)
-print(response)
-soup = BeautifulSoup(response.text,features="lxml")
-kok=soup.findAll('a')
+#Function responsible for sav into desktop directory file with txt format
+def save_all_into_file(file_name: str, url: str):
+    with open(os.path.join('/Users/amajcher/Desktop',file_name),'w+') as Fileproc:
+        for item in creat_file_from_web(url):
+            Fileproc.write('%s\n' % item)
+            
+#Function responsible for searching choseen word from file, and print list of images and list of lnks
+def search_word_from_file(file_name,searching_word):
+    with open(os.path.join('/Users/amajcher/Desktop', file_name),'r') as file:
+        contents = file.read()
+        search_word = searching_word
+        FirstList = contents.split()
+        Linkslist = []
+        Imglist= []
+        for i in FirstList:
+            if search_word.lower() in i:
+                if 'uploads' in i:
+                    Imglist.append(i)
+                elif 'href' in i:
+                    Linkslist.append(i)
+                else:
+                    pass
+        SpliterListlink = set(Linkslist)
+        SpliterListimage = set(Imglist)
+        print("All links contains %s" % search_word)
+        print('\n'.join(SpliterListlink))
+        print("All images contains %s" % search_word)
+        print('\n'.join(SpliterListimage))
 
-#Create file links.txt and add all a tags from website
+save_all_into_file('links2.txt','http://amadeuszmajcher.pl/blog/')
 
-with open('links.txt','w+') as Fileproc:
-    for item in kok:
-        Fileproc.write('%s\n' % item)
+search_word_from_file('links2.txt','trener')
+
+
+
