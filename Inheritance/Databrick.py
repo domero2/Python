152f9c32a812452806c55247059f85b29536108b
domero2 <stefanbelmond@gmail.com>
2020-02-11 16:05:04 +0100

    comments in files

--- Inheritance/Databrick.py
+++ Inheritance/Databrick.py
@@ -1,1 +1,110 @@
+import math
+from inheritance import Astronomer
+from colorama import Fore
+
+
+class Databricks(Astronomer):
+
+    def __init__(self):
+        # Astronomer.__init__(self,task_id='456id')
+        print('databricks created')
+
+
+# Object of databricks class
+firsdatabricks = Databricks()
+
+# Object inheritance from Astronomer method astro_run() method responsible to return sysdate as last astro run
+seconddatabrick = firsdatabricks.astro_run()
+
+
+class SublimeText():
+    def __init__(self, name: str):
+        self.name = name
+
+    def return_input(self):
+        raise NotImplementedError("lolz")
+
+
+# Specjal method which allow printing length as amount of pages and string values from object
+class Book():
+    def __init__(self, title: str, author: str, pages: int):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f" author: {self.author} title:  {self.title} pages: {self.pages}"
+
+    def __len__(self):
+        return self.pages
+
+
+b = Book(title='Somebook', author='Jarek', pages=23)
+
+print(b)
+print(len(b))
+
+#Mathematics class with method counting distance and slope
+class Line():
+    def __init__(self, cor1: tuple, cor2: tuple):
+        self.cor1 = cor1
+        self.cor2 = cor2
+
+    def distance(self):
+        firstarg = (self.cor2[0] - self.cor1[0])
+        secondarg = (self.cor2[1] - self.cor1[1])
+        d = math.pow(firstarg, 2) + math.pow(secondarg, 2)
+        g = math.sqrt(d)
+        return g
+
+    def slope(self):
+        firstarg = (self.cor2[1] - self.cor2[0])
+        secondarg = (self.cor1[1] - self.cor1[0])
+        d = firstarg / secondarg
+        return d
+
+
+cordinate1 = (3, 2)
+cordiante2 = (8, 10)
+koko = Line(cordinate1, cordiante2)
+print(koko.distance())
+print(koko.slope())
+
+
+# Example off bank account system
+class Account():
+    def __init__(self, owner: str, ballance: float):
+        self.owner = owner
+        self.ballance = ballance
+
+    def deposit(self, amount):
+        self.ballance += amount
+        print('Deposit Accepted')
+        return self.ballance
+
+    def withdraw(self, withdraw):
+        if withdraw > self.ballance:
+            print('nor enough founds')
+
+        else:
+            print('Withdraw accepted')
+        self.ballance -= withdraw
+        return self.ballance
+
+    def check_ballance(self):
+        print(f"you have yet: {self.ballance} ammount of money")
+
+    def __str__(self):
+        return f"Owner: {self.ballance}\n Ballance: {self.ballance}"
+
+
+wallet = Account(owner='Jaro', ballance=100)
+
+wallet.deposit(10)
+
+wallet.withdraw(20)
+
+wallet.check_ballance()
+
+print(Fore.RED + "some text")
 
