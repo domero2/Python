968b6c55a7592bb63b4908e7da9c97899f8f52cf
domero2 <stefanbelmond@gmail.com>
2020-02-19 13:58:50 +0100

    added method searching result

--- SEO_tool/Functions.py
+++ SEO_tool/Functions.py
@@ -4,5 +4,10 @@ class onbutton():
     def __init__(self):
         pass
 
-    def click(self, entry):
-        print('Button clicked: ' + entry)
+    def click(self, entry, entry2):
+        print('Button clicked: ' + entry + ' ' + entry2)
+
+    def searching_results(self,object,label):
+        for item in object:
+            label['text'] = '\n'.join(item)
+
